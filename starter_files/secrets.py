a93f91d12547858c2ea65c748ff0ed180dafda38
roshanvino <roshanpreetham21@gmail.com>
2024-05-01 21:22:34 +0100

    refactoring .csv file

--- starter_files/secrets.py
+++ starter_files/secrets.py
@@ -1,1 +1,2 @@
-# this should be in the git-ignore folder but I have include it in the git repo as this project only works with a sandbox api key
+# this should be in the git-ignore folder but I have include it in the git repo as this project only works with a
+# sandbox api key
